f41180a1d457646034c41aec1b689aaa75222b71
qjatn0521 <qjatn0521@kw.ac.kr>
2023-09-19 16:24:22 +0900

    Feat : Fix Bottomenavigation Moving
    바텀 네비게이션 이동 안되던 오류 수정
    
    Resolves: #1

--- app/src/main/java/com/example/clock/MainActivity.kt
+++ app/src/main/java/com/example/clock/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.clock
 
-import android.R
-import android.os.Bundle
-import android.text.SpannableString
-import android.text.style.UnderlineSpan
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.clock.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -23,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fl_container) as NavHostFragment
         val navController = navHostFragment.findNavController()
         binding.bottomNavigationView.setupWithNavController(navController)
-
-
 
 
         setContentView(binding.root)
