af4543193b6d9b9a579caeae3696058ffff3e16c
harry <harry7408@naver.com>
2023-10-08 16:03:56 +0900

    feat : Wheel 및 Menu 추가

--- app/src/main/java/com/example/clock/Fragment/FragmentTimer.kt
+++ app/src/main/java/com/example/clock/Fragment/FragmentTimer.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.clock.databinding.FragmentAlarmBinding
 import com.example.clock.databinding.FragmentTimerBinding
 
 class FragmentTimer : Fragment() {
 
    private lateinit var binding: FragmentTimerBinding
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTimerBinding.inflate(inflater, container, false)
         return binding.root
     }
